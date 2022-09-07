8930bb6d4b9d655c32a857536dba102a4bae3a50
Adriano de Oliveira Cavalcante Ferreira <adriano.ocf@hotmail.com>
2022-09-07 19:59:09 -0300

    quarta-feira 07/9/2022

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -5,16 +5,37 @@ namespace App\Controllers;
 //os recursos do miniframework
 use MF\Controller\Action;
 use MF\Model\Container;
+use APP\Models;
 
 class IndexController extends Action {
 
 	public function index() {
 
-		$this->render('index1');
+		$this->render('index');
 	}
 
 	public function inscreverse() {
 		$this->render('inscreverse');
+	}
+
+	public function registrar()
+	{
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+		
+		$quantUsuarios = $usuario->getUsuarioPorEmail();
+
+		if( ($usuario->validarCadastro()) && (count($quantUsuarios) == 0 ))
+		{
+			$usuario->salvar();
+			$this->render('cadastro');
+		}
+		else
+		{
+
+		}
 	}
 
 }
